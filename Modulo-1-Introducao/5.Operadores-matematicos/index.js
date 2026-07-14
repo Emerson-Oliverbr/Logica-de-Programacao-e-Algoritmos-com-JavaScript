a7d7073151c2bb58d839c38b4cd74026d0969617
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-07-13 21:02:54 -0300

    Operadores matematicos

---
+++ Modulo-1-Introducao/5.Operadores-matematicos/index.js
@@ -0,0 +1,19 @@
+/**
+ * / Divisão
+ * * Multiplicacao
+ * / Divisao
+ * + Adicao
+ * % Modulo
+ * - Subtracao
+ */
+
+let number1 = 10;
+let number2 = 5;
+let resultado;
+
+resultado = number1 + number2;
+console.log(resultado);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+console.log(number1 % number2);

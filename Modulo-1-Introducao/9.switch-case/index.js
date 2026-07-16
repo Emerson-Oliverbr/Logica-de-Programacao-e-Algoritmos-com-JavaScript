a44ff334a762711674b156c675d18b13a860c2bf
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-07-15 21:06:57 -0300

    switch-case

---
+++ Modulo-1-Introducao/9.switch-case/index.js
@@ -0,0 +1,28 @@
+const diaDaSemana = "Quinta#";
+
+switch (diaDaSemana) {
+  case "Domingo":
+    console.log("Final de semana!");
+    break;
+  case "Segunda":
+    console.log("Dia útil!");
+    break;
+  case "Terça":
+    console.log("Dia útil!");
+    break;
+  case "Quarta":
+    console.log("Dia útil!");
+    break;
+  case "Quinta":
+    console.log("Dia útil!");
+    break;
+  case "Sexta":
+    console.log("Dia útil!");
+    break;
+  case "Sabado":
+    console.log("Final de Semana!");
+    break;
+  default:
+    console.log("Dia Inválido!!");
+    break;
+}

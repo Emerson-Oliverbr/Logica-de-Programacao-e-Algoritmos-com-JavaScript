0b2401a1ba1d6c8daf39a717ecb997dbe37b9d30
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-07-22 19:22:38 -0300

    Metodo push

---
+++ Modulo-2-Estruturas-de-Repeticao-e-Arrays/5.Arrays/5.Metodo-push/index.js
@@ -0,0 +1,11 @@
+const frutas = ["Morango", "Laranja", "Lomão", "Kiwi"];
+console.log(frutas);
+
+frutas.push("Uva");
+console.log(frutas);
+
+frutas.push("Manga");
+console.log(frutas);
+
+frutas.push("Morango");
+console.log(frutas);

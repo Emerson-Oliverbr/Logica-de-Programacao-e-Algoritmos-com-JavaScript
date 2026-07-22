0bdd12dbbfa1fce0cea65d6793fb816ed46aa823
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-07-22 19:29:58 -0300

    Metodo pop

---
+++ Modulo-2-Estruturas-de-Repeticao-e-Arrays/5.Arrays/7.Metodo-pop/index.js
@@ -0,0 +1,27 @@
+const frutas = ["Morango", "Laranja", "Lomão", "Kiwi"];
+console.log(frutas);
+
+frutas.push("Uva");
+console.log(frutas);
+
+frutas.push("Manga");
+console.log(frutas);
+
+frutas.push("Morango");
+console.log(frutas);
+
+frutas.unshift("Mamão");
+console.log(frutas);
+
+frutas.unshift("Abacaxi");
+console.log(frutas);
+
+frutas.unshift("Mamão");
+console.log(frutas);
+
+frutas.pop();
+console.log(frutas);
+
+frutas.pop();
+console.log(frutas);
+
